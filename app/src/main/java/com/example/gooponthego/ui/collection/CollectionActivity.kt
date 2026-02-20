72782b3fa1a8e69ff0ca4aaf93299b988b627be1
SquigglyOwl <edwinleezr00@gmail.com>
2026-02-20 14:49:12 +0800

    added back android class

--- app/src/main/java/com/example/gooponthego/ui/collection/CollectionActivity.kt
+++ app/src/main/java/com/example/gooponthego/ui/collection/CollectionActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
